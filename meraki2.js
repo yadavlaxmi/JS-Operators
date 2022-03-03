5a88f71d63a8bb24d7dad893f2ddf013e2a234d9
Laxmi Yadav <91363406+yadavlaxmi@users.noreply.github.com>
2022-03-03 15:13:54 +0530

    Add files via upload

---
+++ meraki2.js
@@ -0,0 +1,12 @@
+var a = 5, b = 10, c = "5";
+var x = a;
+console.log(a === c);
+console.log(a == c);
+console.log(a == x);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a >= c);
+console.log(a <= c);
